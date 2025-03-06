b69edfa2ac4c5a524566abb68d8fe925a373385c
sebahernandez <sebaprogramer@gmail.com>
2025-03-06 20:25:43 -0300

    vercel done

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  output: "server",
+  adapter: vercel(),
 });
